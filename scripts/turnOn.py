ab5c620171af5be77342347f61ecc820ce836cff
root <root@raspberrypi>
2020-01-16 12:49:17 -0800

    started python scripts

---
+++ scripts/turnOn.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as GPIO
+from time import sleep
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(11,GPIO.OUT)
+p = GPIO.PWM(11, 50)
+p.start(0)
+
+p.ChangeDutyCycle(3)
+sleep(1)
+
+p.stop()
+GPIO.cleanup()
