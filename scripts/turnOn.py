2b3144ed1a4fbb64028e3b9e65119e8fb256a6e5
root <root@raspberrypi>
2020-03-08 18:57:30 -0700

    changing things up

--- scripts/turnOn.py
+++ scripts/turnOn.py
@@ -1,15 +1,16 @@
 import RPi.GPIO as GPIO
 from time import sleep
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(11,GPIO.OUT)
 p = GPIO.PWM(11, 50)
-p.start(7.5)
+p.start(2.5)
 
-p.ChangeDutyCycle(2.5)
-sleep(1)
-p.ChangeDutyCycle(12.5)
-sleep(1)
-
-p.stop()
-GPIO.cleanup()
+try:
+    while True:
+        p.ChangeDutyCycle(2.5)
+        time.sleep(1)
+        p.ChangeDutyCycle(12.5)
+        time.sleep(1)
+        p.stop()
+        GPIO.cleanup()
